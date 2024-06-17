79118bdf0b944651f28bf763d7594840b6373bd7
minhminh <luongngocminh>
2024-06-17 23:31:50 +0700

    make main branch empty

--- cypress/component/HeaderExercise.js
+++
@@ -1,7 +0,0 @@
-export default class HeaderExercise {
-    getSignUpLink = () => cy.contains('Signup / Login')
-    getDeleteAccountBtn = () => cy.get('.fa.fa-trash-o')
-    getTestCaseBtn = () => cy.contains('Test Cases')
-    getProductBtn = () => cy.get('.material-icons.card_travel')
-    getCartBtn = () => cy.get('.fa.fa-shopping-cart')
-}
